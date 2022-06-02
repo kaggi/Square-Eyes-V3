259bcd9f887e0253fe92fd07e0a7fc68a591385d
kaggi <lola.lohne@gmail.com>
2022-06-02 12:13:42 +0200

    film page

--- js/search.js
+++ js/search.js
@@ -1,9 +1,9 @@
-console.log("Hello from search.js");
-
 let searchLink = document.querySelector("#search_link");
 searchLink.classList.add("current_page");
 
-const search_results_section = document.querySelector(".search_results_section");
+const search_results_section = document.querySelector(
+  ".search_results_section"
+);
 const search = document.querySelector("#search_input");
 const searchFieldError = document.querySelector("#searchFieldError");
 
@@ -11,35 +11,30 @@ const button = document.querySelector("#search_button");
 
 let searchOK = false;
 
-function checkSearchField (event) {
-    if(testLength(search.value, 0)) {
-        searchOK = true;
-        searchFieldError.style.display = "none";
-    }
-    else {
-        searchFieldError.style.display = "block";
+function checkSearchField(event) {
+  if (testLength(search.value, 0)) {
+    searchOK = true;
+    searchFieldError.style.display = "none";
+  } else {
+    searchFieldError.style.display = "block";
+  }
 
-    }
+  if (searchOK) {
+    search_results_section.style.display = "flex";
+    search_results_section.style.flexDirection = "column";
 
-    if(searchOK) {
-        search_results_section.style.display = "flex"; 
-        search_results_section.style.flexDirection = "column"; 
-
-        searchOK = false;
-    }
-    else {
-        search_results_section.style.display = "none";
-    }
+    searchOK = false;
+  } else {
+    search_results_section.style.display = "none";
+  }
 }
 
-
 function testLength(value, testValue) {
-    if (value.trim().length > testValue) {        
-        return true;
-    }
-    else {        
-        return false;
-    }
+  if (value.trim().length > testValue) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 button.addEventListener("click", checkSearchField);
